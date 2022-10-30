0aeb1c523cf544745530f487dd56fd017e4cc5e8
Ashkhen-Adamyan <salaxyan1989@mail.ru>
2022-10-30 19:38:59 +0300

    внесла правки

--- js/main.js
+++ js/main.js
@@ -1,48 +1,46 @@
 /*burger*/
-let menuBtn = document.querySelector('.header__burger');
-let menu = document.querySelector('.header__burger-menu');
-let body = document.querySelector('body');
-let html = document.querySelector('html');
-let link = document.querySelectorAll('.header__burger-link')
+let menuBtn = document.querySelector(".header__burger");
+let menu = document.querySelector(".header__burger-menu");
+let body = document.querySelector("body");
+let html = document.querySelector("html");
+let link = document.querySelectorAll(".header__burger-link");
 
-menuBtn.addEventListener('click', function () {
-  menuBtn.classList.toggle('header__burger-active');
-  menu.classList.toggle('header__burger-menu-active');
-  body.classList.toggle('overflow--lock');
-  html.classList.toggle('overflow--lock');
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("header__burger-active");
+  menu.classList.toggle("header__burger-menu-active");
+  body.classList.toggle("overflow--lock");
+  html.classList.toggle("overflow--lock");
 
-  link.forEach(el => {
-    el.addEventListener('click', () => {
-      body.classList.remove('overflow--lock')
-      html.classList.remove('overflow--lock')
-      menuBtn.classList.remove('header__burger-active')
-      menu.classList.remove('header__burger-menu-active')
-    })
-  })
+  link.forEach((el) => {
+    el.addEventListener("click", () => {
+      body.classList.remove("overflow--lock");
+      html.classList.remove("overflow--lock");
+      menuBtn.classList.remove("header__burger-active");
+      menu.classList.remove("header__burger-menu-active");
+    });
+  });
 });
 
 /*search*/
 
-let searchBtn = document.querySelector('.header__searth-btn');
-let searchForm = document.querySelector('.header__search-top');
-let searchFormClose = document.querySelector('.header__searth-close');
+let searchBtn = document.querySelector(".header__searth-btn");
+let searchForm = document.querySelector(".header__search-top");
+let searchFormClose = document.querySelector(".header__searth-close");
 
-searchBtn.addEventListener('click', function () {
-  searchForm.classList.add('header__searth-active');
-})
-
-
-searchFormClose.addEventListener('click', function () {
-  searchForm.classList.remove('header__searth-active');
+searchBtn.addEventListener("click", function () {
+  searchForm.classList.add("header__searth-active");
 });
 
+searchFormClose.addEventListener("click", function () {
+  searchForm.classList.remove("header__searth-active");
+});
 
 // header dropdawn
 const params = {
   btnClassName: "js-header-dropdown-btn",
   dropClassName: "js-header-drop",
   activeClassName: "is-active",
-  disabledClassName: "is-disabled"
+  disabledClassName: "is-disabled",
 };
 
 function onDisable(evt) {
@@ -95,100 +93,114 @@ function setMenuListener() {
 
 setMenuListener();
 // scroll
-const simpleBar1 = new SimpleBar(document.getElementById('scroll-1'),
-  { autoHide: true, scrollbarMaxSize: 28 }); simpleBar1.recalculate();
+const simpleBar1 = new SimpleBar(document.getElementById("scroll-1"), {
+  autoHide: true,
+  scrollbarMaxSize: 28,
+});
+simpleBar1.recalculate();
 
-const simpleBar2 = new SimpleBar(document.getElementById('scroll-2'),
-  { autoHide: false, scrollbarMaxSize: 28 }); simpleBar2.recalculate();
+const simpleBar2 = new SimpleBar(document.getElementById("scroll-2"), {
+  autoHide: false,
+  scrollbarMaxSize: 28,
+});
+simpleBar2.recalculate();
 
-const simpleBar3 = new SimpleBar(document.getElementById('scroll-3'),
-  { autoHide: false, scrollbarMaxSize: 28 }); simpleBar3.recalculate();
+const simpleBar3 = new SimpleBar(document.getElementById("scroll-3"), {
+  autoHide: false,
+  scrollbarMaxSize: 28,
+});
+simpleBar3.recalculate();
 
-const simpleBar4 = new SimpleBar(document.getElementById('scroll-4'),
-  { autoHide: false, scrollbarMaxSize: 28 }); simpleBar4.recalculate();
+const simpleBar4 = new SimpleBar(document.getElementById("scroll-4"), {
+  autoHide: false,
+  scrollbarMaxSize: 28,
+});
+simpleBar4.recalculate();
 
-const simpleBar5 = new SimpleBar(document.getElementById('scroll-5'),
-  { autoHide: false, scrollbarMaxSize: 28 }); simpleBar5.recalculate();
+const simpleBar5 = new SimpleBar(document.getElementById("scroll-5"), {
+  autoHide: false,
+  scrollbarMaxSize: 28,
+});
+simpleBar5.recalculate();
 
 // hero swiper
-const heroSwiper = new Swiper('.hero__swiper-container', {
+const heroSwiper = new Swiper(".hero__swiper-container", {
   allowTouchMove: false,
   loop: true,
-  effect: 'fade',
+  effect: "fade",
   speed: 6000,
   autoplay: {
-    delay: 2000
-  }
+    delay: 2000,
+  },
 });
 
 // swiper gallery
-var swiper = new Swiper('.gallery__swiper', {
+const swiper = new Swiper(".gallery__swiper", {
   slidesPerView: 3,
   slidesPerGroup: 3,
-  loop: true,
+  loop: false,
   spaceBetween: 50,
   // пагинация
   pagination: {
-    el: '.gallery__swiper-pagination',
+    el: ".gallery__swiper-pagination",
     clickable: true,
-    type: 'fraction'
+    type: "fraction",
   },
   // навигация
   navigation: {
-    nextEl: '.gallery__swiper-button-next',
-    prevEl: '.gallery__swiper-button-prev',
+    nextEl: ".gallery__swiper-button-next",
+    prevEl: ".gallery__swiper-button-prev",
   },
 
   breakpoints: {
     320: {
       slidesPerView: 1,
-      slidesPerGroup: 1
+      slidesPerGroup: 1,
     },
 
     572: {
       slidesPerView: 2,
       slidesPerGroup: 2,
-      spaceBetween: 10
+      spaceBetween: 10,
     },
 
     768: {
       slidesPerView: 2,
       slidesPerGroup: 2,
-      spaceBetween: 35
+      spaceBetween: 35,
     },
 
     1024: {
       slidesPerView: 2,
       slidesPerGroup: 2,
-      spaceBetween: 34
+      spaceBetween: 34,
     },
 
     1210: {
       slidesPerView: 3,
       slidesPerGroup: 3,
-      spaceBetween: 50
-    }
+      spaceBetween: 50,
+    },
   },
 
   a11y: false,
   keyboard: {
     enabled: true,
-    onlyInViewport: true
+    onlyInViewport: true,
   },
   watchSlidesProgress: true,
   watchSlidesVisibility: true,
   slideVisibleClass: "slide-visible",
-
 });
 
 // select on gallery
 
-const element = document.querySelector('#selectCustom');
+const element = document.querySelector("#selectCustom");
 const choices = new Choices(element, {
   searchEnabled: false,
-  position: 'bottom',
+  position: "bottom",
   placeholder: true,
-  itemSelectText: ''
+  itemSelectText: "",
 });
 
 // modal on gallery
@@ -198,85 +210,87 @@ const modal = new GraphModal();
 
 (() => {
   new Accordion(".js-accordion-container", {
-    openOnInit: [0]
+    openOnInit: [0],
   });
 })();
 
 // tabs on catalog
 
-document.querySelectorAll('.catalog__list-item').forEach(function (tabsBtn) {
-  tabsBtn.addEventListener('click', function (e) {
+document.querySelectorAll(".catalog__list-item").forEach(function (tabsBtn) {
+  tabsBtn.addEventListener("click", function (e) {
     const path = e.currentTarget.dataset.path;
 
-    document.querySelectorAll('.catalog__list-item').forEach(function (btn) {
-      btn.classList.remove('catalog__list-active')
+    document.querySelectorAll(".catalog__list-item").forEach(function (btn) {
+      btn.classList.remove("catalog__list-active");
     });
-    e.currentTarget.classList.add('catalog__list-active');
+    e.currentTarget.classList.add("catalog__list-active");
 
-    document.querySelectorAll('.catalog__artist').forEach(function (tabsBtn) {
-      tabsBtn.classList.remove('catalog__active')
+    document.querySelectorAll(".catalog__artist").forEach(function (tabsBtn) {
+      tabsBtn.classList.remove("catalog__active");
     });
 
-    document.querySelector(`[data-target="${path}"]`).classList.add('catalog__active');
+    document
+      .querySelector(`[data-target="${path}"]`)
+      .classList.add("catalog__active");
   });
 });
 
 // swiper on events
 
-const eventsSwiper = new Swiper('.events__swiper-container', {
+const eventsSwiper = new Swiper(".events__swiper-container", {
   slidesPerView: 3,
   slidesPerGroup: 3,
   grid: {
     rows: 1,
-    fill: "row"
+    fill: "row",
   },
 
   spaceBetween: 50,
 
   pagination: {
-    el: '.events__swiper-pagination',
-    clickable: true
+    el: ".events__swiper-pagination",
+    clickable: true,
   },
 
   navigation: {
-    nextEl: '.events__swiper-button-next',
-    prevEl: '.events__swiper-button-prev',
+    nextEl: ".events__swiper-button-next",
+    prevEl: ".events__swiper-button-prev",
   },
 
   breakpoints: {
     320: {
       slidesPerView: 1,
-      slidesPerGroup: 1
+      slidesPerGroup: 1,
     },
 
     650: {
       slidesPerView: 2,
       slidesPerGroup: 1,
-      spaceBetween: 30
+      spaceBetween: 30,
     },
 
     768: {
       slidesPerView: 2,
       slidesPerGroup: 2,
-      spaceBetween: 32
+      spaceBetween: 32,
     },
 
     1024: {
       slidesPerView: 3,
       slidesPerGroup: 3,
-      spaceBetween: 30
+      spaceBetween: 30,
     },
 
     1920: {
       slidesPerView: 3,
-      spaceBetween: 50
-    }
+      spaceBetween: 50,
+    },
   },
 
   a11y: false,
   keyboard: {
     enabled: true,
-    onlyInViewport: true
+    onlyInViewport: true,
   },
   on: {
     init: function () {
@@ -289,6 +303,7 @@ const eventsSwiper = new Swiper('.events__swiper-container', {
       });
     },
     slideChange: function () {
+      console.log();
       this.slides.forEach((slide) => {
         if (!slide.classList.contains("slide-visible")) {
           slide.tabIndex = "-1";
@@ -296,14 +311,14 @@ const eventsSwiper = new Swiper('.events__swiper-container', {
           slide.tabIndex = "";
         }
       });
-    }
-  }
+    },
+  },
 });
 
 /*tooltip on projects*/
 
 (() => {
-  tippy('.js-tooltip', {
+  tippy(".js-tooltip", {
     theme: "projects-tooltip",
     maxWidth: 264,
   });
@@ -311,79 +326,78 @@ const eventsSwiper = new Swiper('.events__swiper-container', {
 
 // swiper on projects
 
-const projectsSwiper = new Swiper('.projects__swiper-container', {
+const projectsSwiper = new Swiper(".projects__swiper-container", {
   slidesPerView: 3,
   slidesPerGroup: 1,
   loop: true,
   spaceBetween: 50,
   // пагинация
   pagination: {
-    el: '.projects__swiper-pagination',
+    el: ".projects__swiper-pagination",
   },
   // навигация
   navigation: {
-    nextEl: '.projects__swiper-button-next',
-    prevEl: '.projects__swiper-button-prev',
+    nextEl: ".projects__swiper-button-next",
+    prevEl: ".projects__swiper-button-prev",
   },
 
   breakpoints: {
     320: {
-      slidesPerView: 1
+      slidesPerView: 1,
     },
 
     571: {
       slidesPerView: 2,
-      spaceBetween: 20
+      spaceBetween: 20,
     },
 
     768: {
       slidesPerView: 2,
       slidesPerGroup: 2,
-      spaceBetween: 32
+      spaceBetween: 32,
     },
 
     1024: {
       slidesPerView: 2,
       slidesPerGroup: 3,
-      spaceBetween: 50
+      spaceBetween: 50,
     },
 
     1210: {
       slidesPerView: 3,
       slidesPerGroup: 3,
-      spaceBetween: 50
-    }
+      spaceBetween: 50,
+    },
   },
-
-
 });
-
 
 // map
 
 ymaps.ready(init);
 function init() {
-  const mapElem = document.querySelector('#map');
-  const myMap = new ymaps.Map("map",
+  const mapElem = document.querySelector("#map");
+  const myMap = new ymaps.Map(
+    "map",
     {
       center: [55.75846806898367, 37.60108849999989],
       zoom: 17,
-      controls: ['geolocationControl', 'zoomControl']
+      controls: ["geolocationControl", "zoomControl"],
     },
     {
       suppressMapOpenBlock: true,
       geolocationControlSize: "large",
       geolocationControlPosition: { top: "200px", right: "20px" },
-      geolocationControlFloat: 'none',
+      geolocationControlFloat: "none",
       zoomControlSize: "small",
       zoomControlFloat: "none",
-      zoomControlPosition: { top: "120px", right: "20px" }
+      zoomControlPosition: { top: "120px", right: "20px" },
     }
   );
 
-  myMap.behaviors.disable('scrollZoom');
+  myMap.behaviors.disable("scrollZoom");
 
-  const myPlacemark = new ymaps.Placemark([55.75846806898367, 37.60108849999989],
+  const myPlacemark = new ymaps.Placemark(
+    [55.75846806898367, 37.60108849999989],
     {},
     {
       iconLayout: "default#image",
@@ -403,41 +417,39 @@ var selector = document.querySelector("input[type='tel']");
 var im = new Inputmask("+7(999)-999-99-99");
 im.mask(selector);
 
-
-const validation = new JustValidate('.contacts__form');
+const validation = new JustValidate(".contacts__form");
 
 validation
-  .addField('#name', [
+  .addField("#name", [
     {
-      rule: 'required',
-      errorMessage: 'Как вас зовут?',
+      rule: "required",
+      errorMessage: "Как вас зовут?",
     },
     {
-      rule: 'minLength',
+      rule: "minLength",
       value: 3,
-      errorMessage: 'Имя слишком короткое',
+      errorMessage: "Имя слишком короткое",
     },
     {
-      rule: 'maxLength',
+      rule: "maxLength",
       value: 30,
-      errorMessage: 'Имя слишком длинное',
+      errorMessage: "Имя слишком длинное",
     },
   ])
 
-  .addField('#number', [
+  .addField("#number", [
     {
-      rule: 'required',
-      errorMessage: 'Укажите ваш телефон',
+      rule: "required",
+      errorMessage: "Укажите ваш телефон",
     },
     {
-      rule: 'minLength',
+      rule: "minLength",
       value: 11,
-      errorMessage: 'Неверный формат',
+      errorMessage: "Неверный формат",
     },
+  ]);
 
-  ])
-
-form.addEventListener('submit', e => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(form);
 
@@ -445,14 +457,7 @@ form.addEventListener('submit', e => {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
-  })
-    .catch((error) => console.log('Sending form failed'));
+  }).catch((error) => console.log("Sending form failed"));
 
   e.target.reset();
-
 });
-
-
-
-
-
